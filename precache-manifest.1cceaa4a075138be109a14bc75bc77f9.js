90dfa8759ddc08165663c31120febdfb37b34600
katnovoa <cnovoa83@gmail.com>
2018-12-20 23:14:20 -0800

    Updates

---
+++ precache-manifest.1cceaa4a075138be109a14bc75bc77f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f6ea29c434cbe641d5b",
+    "url": "/reactnd-project-myreads/static/css/main.98eed1b4.chunk.css"
+  },
+  {
+    "revision": "4f6ea29c434cbe641d5b",
+    "url": "/reactnd-project-myreads/static/js/main.4f6ea29c.chunk.js"
+  },
+  {
+    "revision": "b376ca69a38deb772432",
+    "url": "/reactnd-project-myreads/static/js/1.b376ca69.chunk.js"
+  },
+  {
+    "revision": "d2f8cf40e53ea2d3746f",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.d2f8cf40.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "caa004ab0ef9c7fa9e885eb1aa65fd46",
+    "url": "/reactnd-project-myreads/index.html"
+  }
+];
