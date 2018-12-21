4f579fb58979e7b8c8a1182447856b29a836eec4
katnovoa <cnovoa83@gmail.com>
2018-12-20 23:50:28 -0800

    Updates

--- precache-manifest.1cceaa4a075138be109a14bc75bc77f9.js
+++ precache-manifest.1cceaa4a075138be109a14bc75bc77f9.js
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/reactnd-project-myreads/static/js/runtime~main.d2f8cf40.js"
   },
   {
-    "revision": "3687aa988bec28f17ae39153db1646c3",
-    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
   },
   {
     "revision": "78c6f53b2342643639fe1cbaf171bf47",
     "url": "/reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
   },
   {
-    "revision": "bb9363b86b02777d23523851826414e0",
-    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
   },
   {
     "revision": "caa004ab0ef9c7fa9e885eb1aa65fd46",
